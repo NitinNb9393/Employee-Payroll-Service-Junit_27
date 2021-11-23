7ee2949ecf1fb73463d9700a53499c35617fde68
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-23 16:23:12 +0530

    Added:Program for the read and write the employee payroll to a console

---
+++ src/test/java/com/bridgelabz/employeepayroll/Employee_Payroll_Service_Junit/AppTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.employeepayroll.Employee_Payroll_Service_Junit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
